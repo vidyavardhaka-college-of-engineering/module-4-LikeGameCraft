57c835bc0cd9d9d162a8bb73ef0f91213cc6cad6
Abhinav K <84765669+LikeGameCraft@users.noreply.github.com>
2021-08-23 19:12:27 +0300

    Update Program-10.c

--- Program-10.c
+++ Program-10.c
@@ -9,15 +9,16 @@ int main()
    int arr1[25], i,n;
    printf("Input the number of elements to store in the array :");
    scanf("%d",&n);
-   printf("Input elements of the array :\n",n);
+   printf("input elements of the array :%d\n",n);
    for(i=0;i<n;i++)
       {
 	   scanf(" %d",&arr1[i]);
 	  }
    printf("The elements you entered are : \n");
-   for(i=1;i<n;i++)
+   for(i=0;i<n;i++)
       {
-          printf("%d",*(arr1+i));
+	 //print the array elements
+   printf("%d ",arr1[i]);
 	  }
 	   return 0;
 }
